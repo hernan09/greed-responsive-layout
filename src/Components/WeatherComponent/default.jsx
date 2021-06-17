cba40dfdcc9b2cc5cd055d76f8bb477942a65846
hernan <42286312+hernan09@users.noreply.github.com>
2021-06-17 16:54:04 -0300

    asd

--- src/Components/WeatherComponent/default.jsx
+++ src/Components/WeatherComponent/default.jsx
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import ReactLoading from 'react-loading';
 import './_default.css';
+const Drizzle = 'https://images-cienradios-arc.s3.amazonaws.com/radiomitre/weather/llovizna.svg';
+const Nublado = 'https://images-cienradios-arc.s3.amazonaws.com/radiomitre/weather/nublado.svg';
+const NubladoConSol = 'https://images-cienradios-arc.s3.amazonaws.com/radiomitre/weather/nubladoconsol.svg';
+const Clear = 'https://images-cienradios-arc.s3.amazonaws.com/radiomitre/weather/soleado.svg';
+const Thunderstorm = 'https://images-cienradios-arc.s3.amazonaws.com/radiomitre/weather/tormenta.svg';
+const Ventoso = 'https://images-cienradios-arc.s3.amazonaws.com/radiomitre/weather/ventoso.svg';
 
 const WeatherComponent = () => {
   const [lati, setLati] = useState(0);
@@ -12,6 +18,8 @@ const WeatherComponent = () => {
   const [Clima, setClima] = useState('');
   const [Icon, setIcon] = useState('');
   const [Loading, setLoading] = useState(true);
+  const [ id , setId ] = useState('')
+  const [ City , setCity ] = useState('')
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(function (position) {
@@ -36,6 +44,8 @@ const WeatherComponent = () => {
             setMin(Math.round(result.main.temp_min - 273));
             setCountry(result.sys.country);
             setClima(result.weather[0].description);
+            setId(result.weather[0].id)
+            setCity(result.name)
             console.log(result);
           })
           .catch((error) => console.log('error', error));
@@ -43,6 +53,47 @@ const WeatherComponent = () => {
     });
   });
   //api.openweathermap.org/data/2.5/weather?lat=-34.7008819&lon=-58.36031500000001&appid=f20b66a33b32ecfc690c95d0e610f495
+
+ const getWeatherIcon = rangedId => {
+    switch (true) {
+    case rangedId >= 200 && rangedId <= 232:
+      return <img className="positionIcon" src={Thunderstorm} />;
+    case rangedId >= 300 && rangedId <= 321:
+      return <img src={Drizzle} className="positionIcon" />;
+    case rangedId >= 500 && rangedId <= 531:
+      return <img src={NubladoConSol} className="positionIcon" />;
+    case rangedId >= 600 && rangedId <= 622:
+      return <img src={Nublado} className="positionIcon" />;
+    case rangedId >= 701 && rangedId <= 781:
+      return <img src={Ventoso} className="positionIcon" />;
+    case rangedId === 800:
+      return <img src={Clear} className="positionIcon" />;
+    case rangedId >= 801 && rangedId <= 804:
+      return <img src={Clear} className="positionIcon" />;
+    default:
+      return <img src={NubladoConSol} className="positionIcon" />;
+    }
+  };
+  const TransformClimaToSpanish = rangedId => {
+    switch (true) {
+    case rangedId >= 200 && rangedId <= 232:
+      return 'tormenta con lluvia';
+    case rangedId >= 300 && rangedId <= 321:
+      return 'llovizna';
+    case rangedId >= 500 && rangedId <= 531:
+      return 'Lluvia fuerte';
+    case rangedId >= 600 && rangedId <= 622:
+      return 'Nieve';
+    case rangedId >= 701 && rangedId <= 781:
+      return 'Neblina';
+    case rangedId === 800:
+      return 'Cielo Despejado';
+    case rangedId >= 801 && rangedId <= 804:
+      return 'Nubes Dispersas';
+    default:
+      return 'Cielo Despejado';
+    }
+  };
 
   const MinmaxTemp = () => {
     return (
@@ -63,18 +114,14 @@ const WeatherComponent = () => {
       ) : (
         <div className="container_weather">
           <h1>
-            Country <span>{Country}</span>
+          <span>{City}</span>
           </h1>
           <span>
-            {' '}
-            <i
-              className="wi wi-day-rain display-1 fa-lg positionIcon"
-              color="white"
-            ></i>
+            {getWeatherIcon(id)}
             <h2 className="py-2 temp_data">Temp: {Temp}&deg;C</h2>
             {MinmaxTemp()}
           </span>
-          <h3 className="py-3 content_clima">{Clima}</h3>
+          <h3 className="py-3 content_clima">{TransformClimaToSpanish(id)}</h3>
         </div>
       )}
     </div>
