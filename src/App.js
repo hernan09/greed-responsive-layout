0bb580462486eead6e72864d8ab3a71103dc0547
hernan <42286312+hernan09@users.noreply.github.com>
2021-08-19 17:50:11 -0300

    dasd

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Suspense, lazy } from "react";
+import React, { Suspense, lazy } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Navbar from "./Components/Navbar/index.jsx";
@@ -15,6 +15,7 @@ function App() {
         <Navbar />
         <Suspense fallback={<div>Loading...</div>}>
           <Switch>
+            <Route exact path="/" component={Home} />
             <Route exact path="/home" component={Home} />
             <Route exact path="/about" component={About} />
             <Route exact path="/users" component={Users} />
